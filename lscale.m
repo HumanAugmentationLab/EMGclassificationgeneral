5f270871de3ddab42f5e4b0e3eba03970a341ad8
declanketchum <60332824+declanketchum@users.noreply.github.com>
2020-06-25 18:34:50 -0500

    Updated L-scale function

--- lscale.m
+++ lscale.m
@@ -1,28 +1,29 @@
-function [L] = lscale(data)
+function [L] = lscale(X)
 %L-Scale calculations
 %   L-scale is a calculation based on l-moments (second l-moment). Useful
 %   because it is less effected by outliers then other data features. In
 %   this code data can be a two dimensional matrix where each row is the
 %   data of each trial. 
 
-[rows, col] = size(data);
-X = sort(data, 2); %sorts data points from lowest to highest along the rows 
-b0 = mean(data, 2);
-b = zeros(rows, 1)
-l = zeros(1, rows)
+[rows, col] = size(X);
+X = sort(X); %sorts elements in each column from lowest to highest
+b0 = mean(X); %creates a vector holding the mean of each column/trial
+b = zeros(col, 1);
+l = zeros(1, col);
 
-Num = 1:col-1
-Den = col-1
+Num = 1:rows-1; %array filled with integers from 1 to one less then the amount of rows/data points
+Den = rows-1;
 
-for n = 1:rows
-    b(n) = 1/col * sum((Num/Den .* data(n,2:col)));
+for n = 1:col
+    b(n) = 1/rows * sum((Num/Den .* X(2:rows,n)')); %
 end 
 
-tB = [b0 b]'
-Coeff = [-1; 2] %changed this from LegendreShiftPoly(i)
+tB = [b0' b]';
+Coeff = [-1; 2]; %changed this from LegendreShiftPoly(i)
 
-for i = 1:rows
-    l(i) = sum((Coeff.*tB(:,1)),"all");
+
+for i = 1:col
+    l(i) = sum((Coeff.*tB(:,i)),"all");
 end 
 L = l
 
