d5e7c4cfcd0bdd34ef8eeeaf5a189086139921b3
rishitasarin <56645125+rishitasarin@users.noreply.github.com>
2020-06-25 16:41:40 -0400

    tried to add lscale feature

---
+++ lscale.m
@@ -0,0 +1,29 @@
+function [L] = lscale(data)
+%L-Scale calculations
+%   L-scale is a calculation based on l-moments (second l-moment). Useful
+%   because it is less effected by outliers then other data features. In
+%   this code data can be a two dimensional matrix where each row is the
+%   data of each trial. 
+
+[rows, col] = size(data);
+X = sort(data, 2); %sorts data points from lowest to highest along the rows 
+b0 = mean(data, 2);
+b = zeros(rows, 1)
+l = zeros(1, rows)
+
+Num = 1:col-1
+Den = col-1
+
+for n = 1:rows
+    b(n) = 1/col * sum((Num/Den .* data(n,2:col)));
+end 
+
+tB = [b0 b]'
+Coeff = [-1; 2] %changed this from LegendreShiftPoly(i)
+
+for i = 1:rows
+    l(i) = sum((Coeff.*tB(:,1)),"all");
+end 
+L = l
+
+end
