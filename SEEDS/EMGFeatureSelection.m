449cee021d3b0465a2c89205ecfffde48e4f221c
declanketchum <60332824+declanketchum@users.noreply.github.com>
2020-07-23 08:32:48 -0500

    Update EMGFeatureSelection.m

--- SEEDS/EMGFeatureSelection.m
+++ SEEDS/EMGFeatureSelection.m
@@ -1,23 +1,28 @@
 % Feature Selection by adding features and checking accuracy 
 
 
-all_features = ;
+all_features = traindata.Properties.VariableNames;
 
 accuracy = 0;
-kept_features = []; %empty list to hold the index of features to keep 
+kept_features = {}; %empty list to hold the index of features to keep 
 %input includedfeatures(kept_features) to a select data function
 
+includedfeatures = {'mav', 'var', 'rms', 'zeros', 'aac'};
+includedchannels = [1:6:126 127:134];
 selectedclassifier = {'linSVMmuli'};
-
-
 response = traindata(:,'labels'); %labels
+
+kval = 9;
+% Predictors are features
+cpart = cvpartition(response{:,1},'KFold',kval); % k-fold stratified cross validation
+
 
 %add features
 for n = 1:length(all_features)
-    kept_features = [kept_features n];
-    predictorNames = select_data(kept_features);
+    kept_features{end+1} = all_features{n};
+    predictorNames = select_data(kept_features, includedfeatures, includedchannels);
     predictors = traindata(:,predictorNames);
-    c =  accuracy(selectedclassifier, predictors, response);%run classification code with kept features and return accuracy:
+    c =  accuracy(selectedclassifier, predictors, response, cpart);%run classification code with kept features and return accuracy:
     %select data using includedfeatures(kept_features) TODO: writt a select
     %data function 
     %run a classification function that uses selected data and outputs a
