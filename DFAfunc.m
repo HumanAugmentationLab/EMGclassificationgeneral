514e50e4780f52dff9dad78bbb405516e78fdb44
MayaSimone <mayasivanandan@mac.com>
2020-07-24 08:44:22 -0600

    Potential fix for DFAfunc

--- DFAfunc.m
+++ DFAfunc.m
@@ -8,7 +8,15 @@ function DFA = DFAfunc(mydata, windowvector)
 
     yk = (cumsum((mydata)-mean(mydata)));
                    
-    [dataPoints, trialNum] = size(yk);
+    [dim1, dim2] = size(yk);
+    
+    if dim1 > dim2
+        dataPoints = dim1;
+        trialNum = dim2;
+    else
+        dataPoints = dim2;
+        trialNum = dim1;
+    end
                                     
     w.totaltimewindow = [1 dataPoints];
     w.timewindowoverlap = 0;
