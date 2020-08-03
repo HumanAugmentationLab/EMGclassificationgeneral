0e1e70c9b784eea4632127d92732f6cdab77345f
declanketchum <60332824+declanketchum@users.noreply.github.com>
2020-08-02 21:26:46 -0500

    Update creatingclassificationfigures.m

--- SEEDS/creatingclassificationfigures.m
+++ SEEDS/creatingclassificationfigures.m
@@ -12,6 +12,10 @@ s=1; %This is here to make loops later
 load(strcat(dir_input,'subj',num2str(subjectnumbers(s),'%02.f'),fname_input,'_speed',includedspeeds{sp},'.mat'))
 
 includedchannels = [1:6:126 127:134]; % [] for all, otherwise this is a vector of channel numbers
+%includedfeatures = {'bp2t20','bp20t40','bp40t56','bp64t80' ,'bp80t110','bp110t256', 'bp256t512',...
+     %rms', 'iemg','mmav1','mpv','var', 'mav', 'aac', 'zeros', 'mfl', 'ssi', 'medianfreq', 'wamp',...
+     %  'lscale', 'dfa', 'wl', 'm2', 'damv' 'dasdv', 'dvarv', 'msr', 'ld', 'meanfreq', 'stdv', 'skew', 'kurt', 'mob'};
+includedfeatures = {'mav', 'var', 'rms', 'zeros', 'aac'};
 
 kval = 9;% %Choose number of folds.
 dotrainandtest = false; % If running on test as well as train
@@ -19,65 +23,42 @@ dotrainandtest = false; % If running on test as well as train
 selectedclassifier = {'linSVMmuli'};
 
 traindata = splitvars(traindata); %split subvariables into independent variable to not anger the classifier.
-
-%% Run classification on on each feature in includedfeatures separately
-acc_includedfeatures = zeros(length(includedfeatures),1);
-
-% Get y (repsonse and set up cross validation partition)
+variablenames = traindata.Properties.VariableNames;
 response = traindata(:,'labels'); %labels
+kval = 9;
 cpart = cvpartition(response{:,1},'KFold',kval); % k-fold stratified cross validation
+
+acc = {}; %cell array to hold accuracies 
+
+for n = 1:length(includedfeatures)
+    %find classification accuracy for each feature individually
+    includedfeature = includedfeatures(n);
+    predictorNames = select_data(variablenames, includedfeature, includedchannels);
+    predictors = traindata(:,predictorNames);
     
-for f = 1:length(includedfeatures)
+    acc{n} = classification_accuracy(selectedclassifier, predictors, response, cpart);
+end 
 
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % Select data for the channels you want
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    if isempty(includedchannels)
-        % Include all features
-        idx_feat = contains(traindata.Properties.VariableNames,'FEAT_'); % Find all of ones that start with FEAT_
-        idx_selfeat = contains(traindata.Properties.VariableNames,strcat('_',includedfeatures{f}));
-        predictorNames = traindata.Properties.VariableNames(idx_selfeat & idx_feat); %column names that are FEAT and are the channels we want to include
-    else 
-        idx_feat = contains(traindata.Properties.VariableNames,'FEAT_'); % Find all of ones that start with FEAT_
-        names_ch = cellstr([repmat('_ch',length(includedchannels),1)  num2str(includedchannels') repmat('_',length(includedchannels),1) ]); 
-        names_ch = strrep(names_ch,' ',''); % Remove spaces
-        idx_ch = contains(traindata.Properties.VariableNames,names_ch);
-        idx_selfeat = contains(traindata.Properties.VariableNames,strcat('_',includedfeatures{f}));
-        predictorNames = traindata.Properties.VariableNames(idx_ch & idx_feat & idx_selfeat); %column names that are FEAT and are the channels we want to include
-    end
+accuracy_table = table(includedfeatures', acc', 'VariableNames', {'Feature', 'Accuracy'});
+sorted_table = sortrows(accuracy_table, 2, 'descend'); %sort the rows of the accuracy
 
+%Create figure of accuracy as features get added 
+sorted_features = table2array(sorted_table(:,1))'
 
-    % Predictors are features
-    clearvars predictors 
-    predictors = traindata(:,predictorNames); %This is the X
+included_features = [];
+accuracy = [];
+for n = 1:length(sorted_features)
+    included_features = [included_features sorted_features(n)];
+    predictorNames2 = select_data(variablenames, included_features, includedchannels);
+    predictors = traindata(:,predictorNames2);
     
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % Select the classifiers you want
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    for cl = 1:length(selectedclassifier)
-        switch selectedclassifier{cl}
-            case 'linSVMmuli'
-                % Linear SVM for more than 2 outputs
-                trainedClassifier = fitcecoc(predictors,response);
-        end
-    end
+    accuracy(n) = classification_accuracy(selectedclassifier, predictors, response, cpart);
+end 
 
-    partitionedModel = crossval(trainedClassifier,'CVPartition',cpart);
-    [validationPredictions, validationScores] = kfoldPredict(partitionedModel);
+idx_sorted_features = 1:length(sorted_features);
+plot(idx_sorted_features, accuracy)
 
-
-    % Cross validation output
-    validationAccuracy = 1 - kfoldLoss(partitionedModel);%, 'LossFun', 'ClassifError');
-    fprintf('\nValidation accuracy = %.2f%%\n', validationAccuracy*100);
-    trainconchart = confusionchart(traindata.labels,validationPredictions);
-    trainconchart.NormalizedValues;
-    validationAccuracy = sum(traindata.labels==validationPredictions)./length(traindata.labels);
-    fprintf('\nValidation accuracy = %.2f%%\n', validationAccuracy*100);
-    
-    acc_includedfeatures(f) = validationAccuracy; %individual feature validation accuracies
-
-end
-
+%table based on the second column which holds the accuracies
 %order features based on validation accuracy
 %add one feature at a time and run classification. record classification in
 %an array 
